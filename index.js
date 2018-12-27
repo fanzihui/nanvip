535bc93fb63bb54234236d4618a574b6a9085279
fan <1025537699@qq.com>
2018-12-27 21:23:31 +0800

    update

--- index.js
+++ index.js
@@ -2,32 +2,43 @@
 // http://nanrenvip.xyz/nvyouku/1-0-0-0-0-0-0.html
 // http://nanrenvip.xyz/nvyouku/1-0-0-0-0-0-1.html
 // http://nanrenvip.xyz/nvyouku/1-0-0-0-0-0-2.html
-const request = require('request')
-const cheerio  = require('cheerio')
-const Female = require('./name')
+const Crawler = require("crawler");
+const List = require('./list')
+const Domain = 'http://nanrenvip.xyz'
 const baseUrl = 'http://nanrenvip.xyz/nvyouku/1-0-0-0-0-0-'
 const zero = 0
 const suffix = '.html'
-const url = baseUrl.concat(zero,suffix)
 const want_page = 54
-request(url,function(err,res,body){
-    if(body == '' || body == undefined){
-        return
+
+var c = new Crawler({
+    maxConnections : 10,
+    forceUTF8:false,
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+            // 获取总页数
+            // let page_total = $('.dede_pages ul').children().last().find('a').attr('href').replace(/(.+\-)(\d{1,4})(\.\w+)/,'$2')
+            // console.log('page_total',page_total)
+            // let spider_page = want_page || page_total
+            // console.log(spider_page)
+            $('.avps_ny ul li').each(function(index,ele){
+                let name = $(this).find('span').text()
+                let link = Domain + $(this).find('a').attr('href')
+                let avatar = Domain + $(this).find('img').attr('data-original')
+                console.log(`index: ${index} name: ${name} \n link ${link} \n avator ${avatar}`)
+                // http://nanrenvip.xyz/nvyouku/ssyouya.html
+                List(link)
+            })
+        }
+        done();
     }
-    let $ = cheerio.load(body)
-    // 获取总页数
-    let page_total = $('.dede_pages ul').children().last().find('a').attr('href').replace(/(.+\-)(\d{1,4})(\.\w+)/,'$2')
-    console.log('page_total',page_total)
-    let spider_page = want_page || page_total
-    console.log(spider_page)
-    for(var i = spider_page ; i <= spider_page+2; i++){
-        (function(i){
-            setTimeout(function(){
-                console.log('开始时间:', Date.now())
-                console.log(baseUrl.concat(i,suffix));
-                Female(baseUrl.concat(i,suffix))
-                console.log('结束时间:', Date.now())
-            },100*i)
-        })(i)
-    }
-})
+});
+let urlArr = []
+for(var i = 0 ; i <= 1; i++){
+    console.log('开始时间:', Date.now())
+    urlArr.push(baseUrl.concat(i,suffix))
+    console.log('结束时间:', Date.now())
+}
+c.queue(urlArr);
--- info.js
+++ info.js
@@ -1,58 +1,61 @@
-const request = require('request')
-const cheerio  = require('cheerio')
+const Crawler = require("crawler");
+const Domain = 'http://nanrenvip.xyz'
 const WorkSql = require('./workSql')
-const baseUrl = 'http://nanrenvip.xyz'
-// const url = 'http://nanrenvip.xyz/fanhaoku/AB1SSNI-279.html'
-
 var app = function(param){
     if(param.name == '' || param.name == undefined){
         return
     }
     let url = param.link
-    request(url,function(err,res,body){
-        if(body == '' || body == undefined){
-            return
+    var c = new Crawler({
+        maxConnections : 10,
+        forceUTF8:false,
+        callback : function (error, res, done) {
+            if(error){
+                console.log(error);
+            }else{
+                var $ = res.$;
+                /**
+                 * title 标题
+                 * work 作品
+                 * fanhao 番号
+                 * issue 发行日期
+                 * duration 播放时长
+                 * director 导演
+                 * coded  类型: 有码/无码
+                 * producer 制作商
+                 * publisher 发行商
+                 * cate 类别
+                 * pic 图片
+                 */
+                let title,fanhao,issue,duration,coded,cate,pic
+                title = $('.artCon p').eq(0).text()
+                fanhao = $('.artCon p').eq(1).text()
+                issue = $('.artCon p').eq(2).text()
+                duration = $('.artCon p').eq(3).text()
+                coded = $('.artCon p').eq(5).text()
+                cate = $('.artCon p').eq(8).text()
+                pic = Domain + $('.artCon p').last().find('img').attr('data-original')
+                function format(val){
+                    return val.split('：')[1]
+                }
+                option = {
+                    name: param.name,
+                    // name: '三上悠亜',
+                    title: format(title),
+                    fanhao: format(fanhao),
+                    issue: format(issue),
+                    duration: format(duration),
+                    coded: format(coded),
+                    cate: format(cate),
+                    pic : pic,
+                    hit: Math.ceil(Math.random()*10),
+                }
+                // console.log('option',option)
+                WorkSql(option)
+            }
+            done();
         }
-        let $ = cheerio.load(body)
-        /**
-         * title 标题
-         * work 作品
-         * fanhao 番号
-         * issue 发行日期
-         * duration 播放时长
-         * director 导演
-         * coded  类型: 有码/无码
-         * producer 制作商
-         * publisher 发行商
-         * cate 类别
-         * pic 图片
-         */
-        let title,fanhao,issue,duration,coded,cate,pic
-        title = $('.artCon p').eq(0).text()
-        fanhao = $('.artCon p').eq(1).text()
-        issue = $('.artCon p').eq(2).text()
-        duration = $('.artCon p').eq(3).text()
-        coded = $('.artCon p').eq(5).text()
-        cate = $('.artCon p').eq(8).text()
-        pic = baseUrl + $('.artCon p').last().find('img').attr('data-original')
-        function format(val){
-            return val.split('：')[1]
-        }
-        option = {
-            name: param.name,
-            // name: '三上悠亜',
-            title: format(title),
-            fanhao: format(fanhao),
-            issue: format(issue),
-            duration: format(duration),
-            coded: format(coded),
-            cate: format(cate),
-            pic : pic,
-            hit: Math.ceil(Math.random()*10),
-        }
-        // console.log('option',option)
-        WorkSql(option)
-    })
+    });
+    c.queue(url);
 }
-
 module.exports = app
